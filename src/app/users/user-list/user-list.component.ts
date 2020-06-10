65b94c18b30914ebf5e8e190312e6f73f0ef8879
Rodolphe <dutchr@live.fr>
2020-06-10 09:47:00 +0200

    service & display

--- src/app/users/user-list/user-list.component.ts
+++ src/app/users/user-list/user-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService} from '../../user.service';
 
 @Component({
   selector: 'app-user-list',
@@ -6,10 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-list.component.css']
 })
 export class UserListComponent implements OnInit {
-
-  constructor() { }
+users;
+  constructor(private userService : UserService) { }
 
   ngOnInit(): void {
+    this.users = this.userService.getUsers()
   }
 
 }
